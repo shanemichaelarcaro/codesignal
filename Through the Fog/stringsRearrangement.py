d0276e0db746e8baa4379646bc267ca54d758c47
Shane Arcaro <smarcaro@gmail.com>
2020-06-06 21:44:31 -0400

    Finishing Through the Fog and moving to Diving Deeper

--- Through the Fog/stringsRearrangement.py
+++ Through the Fog/stringsRearrangement.py
@@ -1,28 +1,10 @@
+from itertools import permutations
+
+def diff(w1, w2):
+    return sum([a[0] != a[1] for a in zip(w1, w2)]) == 1
+
 def stringsRearrangement(inputArray):
-    words = []
-    for word in inputArray:
-        total_value = 0
-        for letter in word:
-            total_value += ord(letter)
-        words.append((total_value, word))
-    words.sort(key=lambda value: value[0])
-    print(words)
-
-    for index in range(len(words) - 1):
-        errors = 0
-        for letter in range(len(words[index][1])):
-            if words[index][1][letter] != words[index + 1][1][letter]:
-                errors += 1
-        if errors != 1:
-                return False
-    return True
-
-    # print(words)
-
-
-#   ['ab', 'bb', 'aa'] => True
-#   ['aba', 'bbb', 'bab] => False
-
-print(stringsRearrangement(['ab', 'bb', 'aa']))
-print(stringsRearrangement(['aba', 'bbb', 'bab']))
-print(stringsRearrangement(['ff', 'gf', 'af', 'ar', 'hf']))
+    for z in permutations(inputArray):
+        if sum([diff(*x) for x in zip(z, z[1:])]) == len(inputArray) - 1:
+            return True
+    return False
